1c825c208db634bfd8b615b6887991b4fa1752ef
arau0428 <132262554+arau0428@users.noreply.github.com>
2023-08-14 17:23:37 +0900

    Add files via upload
    最新版（アニメーションが効きません）

--- js/content_click.js
+++ js/content_click.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $(".click_circle").click(function() {
+        $(".job_box").toggleClass('active');
         $(this).toggleClass('active');
-        $(".job").addClass('active');
     });
 });
