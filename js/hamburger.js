baddc8e9980d62ad770b7bbd5b98be116aaca83f
arau0428 <132262554+arau0428@users.noreply.github.com>
2023-08-14 16:42:52 +0900

    Add files via upload

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,3 +1,11 @@
-$(".openbtn2").click(function () {
-    $(this).toggleClass('active');
-});
+$(document).ready(function() {
+    $(".openbtn").click(function() {
+      $(".overlay").fadeIn();
+      $(".modal-box").fadeIn();
+    });
+  
+    $(".overlay, .modal-box").click(function() {
+      $(".overlay").fadeOut();
+      $(".modal-box").fadeOut();
+    });
+  });
